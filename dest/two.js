007384d3bbe7cbff490bed8de0ca065977ea5a6d
Arjun-singh <arjunsinght057@gmail.com>
2025-01-03 21:50:35 +0530

    third commit

--- dest/two.js
+++ dest/two.js
@@ -9,6 +9,7 @@ const themebuttom = document.querySelector(".Mode"); //select the Button Element
 
 //A function that change the Theme-mode through ternary operator
 const buttonone = themebuttom.addEventListener("click", function () {
+  //The logic of toogle change
   const check =
     darkmode.className == "light"
       ? (darkmode.className = "dark")
