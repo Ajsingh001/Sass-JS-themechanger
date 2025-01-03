4656b2cdd3302ceef702b114aa1fc5a6fe5713f0
Arjun-singh <arjunsinght057@gmail.com>
2025-01-03 21:48:01 +0530

    second commit

--- dest/two.js
+++ dest/two.js
@@ -1,12 +1,14 @@
 //File for Theme-Change Button;
 
 //Globel variable for selecting
-const darkmode = document.querySelector("body");
-let modeclass = darkmode.className;
 
-const themebuttom = document.querySelector(".Mode");
+const darkmode = document.querySelector("body"); //Select the body element
+let modeclass = darkmode.className; //Select the body-className
+
+const themebuttom = document.querySelector(".Mode"); //select the Button Element
+
+//A function that change the Theme-mode through ternary operator
 const buttonone = themebuttom.addEventListener("click", function () {
-  //If you want to replace all the existing class names with a new one:
   const check =
     darkmode.className == "light"
       ? (darkmode.className = "dark")
